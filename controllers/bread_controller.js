fd652e2dadcf0dab7701e49b2cb7f53f53cdb7e7
sara pena <sarap@Saras-MacBook-Air.local>
2024-03-12 19:01:22 -0700

    sending index and show data

---
+++ controllers/bread_controller.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const breads = express.Router()
+const Bread = require('../models/bread.js')
+
+// INDEX
+breads.get('/', (req, res) => {
+  res.send(Bread)
+})
+
+module.exports = breads
