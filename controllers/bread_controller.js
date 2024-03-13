888ff02aeff7f99113946ffd4ce72187bf53bd3b
sara pena <sarap@Saras-MacBook-Air.local>
2024-03-12 20:17:36 -0700

    sending index and show data

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -4,7 +4,15 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+    res.render('index')
+  // res.send(Bread)
 })
 
+
 module.exports = breads
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+    res.send(Bread[req.params.arrayIndex])
+  })
+  
